911e6c8c267966b4570c94d473b27afa524847dc
윤지호 <hoyunji@naver.com>
2025-04-17 21:59:22 +0900

    Initial Project Commit

---
+++ src/test/java/com/illustration/illustration_api/IllustrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illustration.illustration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllustrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
